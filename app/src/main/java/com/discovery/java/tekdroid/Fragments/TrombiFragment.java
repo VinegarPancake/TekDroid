576fee35842e021a8fd604ea088448f9db3de21c
Jamais <johan.paasche@epitech.eu>
2016-02-02 20:55:42 +0100

    restyled fragment_trombi

--- app/src/main/java/com/discovery/java/tekdroid/Fragments/TrombiFragment.java
+++ app/src/main/java/com/discovery/java/tekdroid/Fragments/TrombiFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.discovery.java.tekdroid.APITools.API;
 import com.discovery.java.tekdroid.R;
@@ -21,6 +23,8 @@ public class                TrombiFragment extends Fragment {
     String                  mLocation;
     String                  mCourse;
     String                  mPromo;
+    Spinner                 mLocationSpinner;
+    Spinner                 mPromoSpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +32,18 @@ public class                TrombiFragment extends Fragment {
         try {   mApi._userLogin = this.getArguments().getString("userLogin"); } catch (NullPointerException e) { e.printStackTrace(); }
         try {   mApi._session_token = this.getArguments().getString("sessionToken");} catch (NullPointerException e) { e.printStackTrace(); }
         try {   mFragmentView = inflater.inflate(R.layout.fragment_trombi, container, false);} catch (NullPointerException e) { e.printStackTrace(); }
+        try
+        {
+            mLocationSpinner = (Spinner)mFragmentView.findViewById(R.id.fragment_trombi_location_spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.trombi_locations, android.R.layout.simple_spinner_dropdown_item);
+            mLocationSpinner.setAdapter(adapter);
+        } catch (NullPointerException e ) {e.printStackTrace();}
+        try
+        {
+            mPromoSpinner = (Spinner)mFragmentView.findViewById(R.id.fragment_trombi_promo_spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.trombi_promos, android.R.layout.simple_spinner_dropdown_item);
+            mPromoSpinner.setAdapter(adapter);
+        } catch (NullPointerException e ) {e.printStackTrace();}
 
         return mFragmentView;
     }
